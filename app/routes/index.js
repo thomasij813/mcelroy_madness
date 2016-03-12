72c18fa5860f7527aaf4b6e2a970197965b2d617
Thomas Johnson <thomasij813@gmail.com>
2016-03-12 01:03:01 -0800

    Add horizontal scrolling when title program title is too long

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,6 @@ var parseString = require('xml2js').parseString;
 var Promise = require('promise');
 var api = require('../api/programs.js');
 
-var googleApiKey = 'AIzaSyA12J9ITYBJmtdbs68gwWRWWEm0owoUliY';
-
 var router = express.Router();
 
 router.get('/', function(req, res) {
